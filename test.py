fe8301f52f571badaa243b2c2a7c938836693e2e
gaungalif <gaungalif@gmail.com>
2021-11-07 10:24:02 +0800

    refactor: change the data path

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ from siamese.prod.utils import *
 import fire
 
 def predict(weight_onnx: str = './weights/siamese-10.onnx', 
-            data_dir: str =  '/home/gaungalif/workspace/datasets/omniglot/Alphabet_of_the_Magi', 
+            data_dir: str =  './datasets/omniglot/Alphabet_of_the_Magi', 
             pic_idx: int = 10):
 
     model = SiamesePredictorOnnx(weight=weight_onnx)
-    data_dir = '/home/gaungalif/workspace/datasets/omniglot/Alphabet_of_the_Magi'
+    data_dir = './datasets/omniglot/Alphabet_of_the_Magi'
 
     main_imgs, comp_imgs, labels = image_loader(idx=2,data_dir=data_dir)
     model.predict(main_imgs, comp_imgs, labels, pic_idx)
